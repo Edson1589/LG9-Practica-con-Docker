1ecae6df60db1763e534f017029b47ad9a19f21d
Alexby58 <146767140+Alexby58@users.noreply.github.com>
2026-09-16 09:44:21 -0400

    get

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -2,4 +2,20 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../config/db');
 
+// Endpoint GET /health
+router.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+// Endpoint GET /users
+router.get('/users', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM users ORDER BY id ASC');
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al consultar usuarios' });
+  }
+});
+
 module.exports = router;
--- api/src/server.js
+++ api/src/server.js
@@ -10,21 +10,7 @@ app.use(express.json());
 
 app.use('./routes/users.js', usersRoutes);
 
-// Endpoint GET /health
-app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok' });
-});
 
-// Endpoint GET /users
-app.get('/users', async (req, res) => {
-  try {
-    const { rows } = await pool.query('SELECT * FROM users ORDER BY id ASC');
-    res.json(rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Error al consultar usuarios' });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
